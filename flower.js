5111e57972985a5dfa37c833a78a988549914965
Vadamh <118827540+Vadamh@users.noreply.github.com>
2023-04-29 21:45:42 -0400

    Update flower.js

--- flower.js
+++ flower.js
@@ -1,12 +1,11 @@
 console.clear();
 
 var doc = document;
-var flower = 
-    doc.querySelector('.flower');
-var range =
-    doc.querySelector('.range');
+var flower = doc.querySelector(".flower");
+var range = doc.querySelector(".range");
 
-var petalPartMarkup = '<div class = "box"> \
+var petalPartMarkup =
+  '<div class = "box"> \
                        <div class= "shape"></div>';
 
 var maxParts = 20;
@@ -18,61 +17,59 @@ var partsFontStep = partsFontStepDef;
 var partsFrontStepDef = 25 / maxParts;
 var hueStep = 150 / maxParts;
 
-createFlower ();
+createFlower();
 
 function createFlower() {
   var angle = 360 / maxPetals;
-  
-  for(var i=0; i< maxPetals; i++) {
-      var petal = createPetals();
-      var currAngle = angle * i + 'deg';
-      var transform 'transform: rotateY(' + currAngle + ') rotateX(-30deg) translateZ(9vmin)';
-      petal.setAttribute('style', transform);
-      flower.appendChild( petal ); 
+
+  for (var i = 0; i < maxPetals; i++) {
+    var petal = createPetal();
+    var currAngle = angle * i + "deg";
+    var transform =
+      "transform: rotateY(" + currAngle + ") rotateX(-30deg) translateZ(9vmin)";
+    petal.setAttribute("style", transform);
+    flower.appendChild(petal);
   }
 }
 
 function createPetal() {
   var box = createBox(null, 0);
-  var petal = doc.createElement('div');
-  petal.classList.add('petal');
-  
-  for(var i=1; i <= maxParts; i++){
-      newBox = createBox ( box, i);
-      box = newBox;
+  var petal = doc.createElement("div");
+  petal.classList.add("petal");
+
+  for (var i = 1; i <= maxParts; i++) {
+    newBox = createBox(box, i);
+    box = newBox;
   }
-  petal.appendChild( box );
+  petal.appendChild(box);
   return petal;
 }
 
 function createBox(box, pos) {
-  var fontSize = partsFontStep * (maxParts - pos) + 'vmin';
+  var fontSize = partsFontStep * (maxParts - pos) + "vmin";
   var half = maxParts / 2;
-  var bright = '50';
-  if(pos<half + 1){
-     fontSize = partsFontStep * pos + 'vmin';
-  }else {
-     bright = 10 + 40 / half * (maxParts - pos);
+  var bright = "50";
+  if (pos < half + 1) {
+    fontSize = partsFontStep * pos + "vmin";
+  } else {
+    bright = 10 + (40 / half) * (maxParts - pos);
   }
-  var color = 'hsl(' + hueStep * pos + ', 100%, ' + bright + '%)';
-  var newShape = doc.createElement('div');
-      newShape.classList.add('shape');
-  var newBox = doc.createElement('div');
-      newBox.classList.add('box');
-  var boxStyle = [
-                  'color: ' + color,
-                  'font-size: ' + fontSize
-                  ].join(';');
-      newBox.setAttribute('style', boxStyle);
-  if(box){
-      new.appendChild(box);
+  var color = "hsl(" + hueStep * pos + ", 100%, " + bright + "%)";
+  var newShape = doc.createElement("div");
+  newShape.classList.add("shape");
+  var newBox = doc.createElement("div");
+  newBox.classList.add("box");
+  var boxStyle = ["color: " + color, "font-size: " + fontSize].join(";");
+  newBox.setAttribute("style", boxStyle);
+  if (box) {
+    newBox.appendChild(box);
   }
-  
+
   newBox.appendChild(newShape);
-  
+
   return newBox;
 }
 
-function out(content){
-   console.log(content);
-} 
+function out(content) {
+  console.log(content);
+}
